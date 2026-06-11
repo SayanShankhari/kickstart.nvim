bc90fe10cd06b57f389fb917dca6bd413bdd3b78
Sayan Shankhari <sayan0shankhari@gmail.com>
2026-06-11 20:56:03 +0530

    fix(misc): restore missing theme and options

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,13 +1,11 @@
 return {
-  --[[]]
-  --'EdenEast/nightfox.nvim',
-  --'craftzdog/solarized-osaka.nvim',
-  'ellisonleao/gruvbox.nvim',
-  lazy = false,
-  priority = 1000,
-  config = function()
-    --vim.cmd.colorscheme 'nightfox'
-    --vim.cmd.colorscheme 'solarized-osaka'
-    vim.cmd.colorscheme 'gruvbox'
-  end,
+  vim.pack.add ( { "https://github.com/ellisonleao/gruvbox.nvim" } );
+
+  require ("gruvbox").setup {
+    styles = {
+      comments = { italic = false }, -- Disable italics in comments
+    },
+  },
+
+  vim.cmd.colorscheme ("gruvbox")
 }
